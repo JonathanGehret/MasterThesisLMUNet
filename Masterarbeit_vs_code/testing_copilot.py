698014e3fda3a97898fa36e0c098af18ab720964
JonathanGehret <jonathan@gehrets.de>
2025-11-25 20:59:56 +0100

    added old files before cleaning it up

---
+++ Masterarbeit_vs_code/testing_copilot.py
@@ -0,0 +1,16 @@
+print(3)
+
+
+print(5)
+
+import numpy as np
+import pandas as pd
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#import sklearn
+
+
+for i in range(10):
+    print(i)
+
+
